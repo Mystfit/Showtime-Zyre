1a44526189c62ab0c60e4b98c5b06bab8e8938a8
Mystfit <byronated@gmail.com>
2016-11-15 11:07:50 +1300

    Added unity project. Designed for 5.5b9 dotnet46 build

---
+++ unity/Assets/ZstActor.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Showtime_Zyre;
+
+public class ZstActor : MonoBehaviour {
+
+    public ZstEndpoint endpoint;
+    private bool _running;
+
+    private Queue<string> messageQueue;
+
+    // Use this for initialization
+    void Start() {
+        messageQueue = new Queue<string>();
+
+        endpoint = new ZstEndpoint("unityzyre", (s) => { Debug.Log("INFO: " + s + "\n"); });
+        endpoint.incoming += Enqueue; //Debug.Log("INCOMING: " + s);
+
+        _running = true;
+        StartCoroutine(Timer());
+    }
+
+    private void Enqueue(string s)
+    {
+        messageQueue.Enqueue("INCOMING: " + s);
+    }
+
+    IEnumerator Timer()
+    {
+        string greeting = "Hello";
+        while (_running)
+        {
+            yield return new WaitForSeconds(2);
+            endpoint.Send(greeting);
+        }
+    }
+
+    public void Update()
+    {
+        try
+        {
+            Debug.Log(messageQueue.Dequeue());
+        }
+        catch (InvalidOperationException e)
+        {
+            
+        }
+    }
+
+    public void OnApplicationQuit()
+    {
+        _running = false;
+        endpoint.Close();
+    }
+}
